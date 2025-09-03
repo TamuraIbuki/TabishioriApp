1b6a225ecbe688212f3d507d4bbf1ac863ebd115
TAMURA IBUKI <ibu0414@yahoo.co.jp>
2025-09-04 01:03:03 +0900

    しおり編集後のOKボタンの削除

--- TabishioriApp/View/EditShioriViewController.swift
+++ TabishioriApp/View/EditShioriViewController.swift
@@ -378,38 +378,26 @@ final class EditShioriViewController: UIViewController {
                 guard let self = self else { return }
                 print("Object added successfully")
                 let alert = UIAlertController(title: "更新しました", message: nil, preferredStyle: .alert)
-                var finished = false
-                let finish: () -> Void = { [weak self] in
-                    guard let self = self, !finished else { return }
-                    finished = true
-                    self.delegate?.didSaveNewShiori(self.dataModel)
-                    
-                    // モーダルを閉じる
-                    let closeModal = {
+                self.present(alert, animated: true)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+                    guard let self = self else { return }
+                    let closeModal: () -> Void = {
                         if let nav = self.navigationController {
                             nav.dismiss(animated: true)
+                            self.delegate?.didSaveNewShiori(self.dataModel)
+                    
                         } else {
-                            self.dismiss(animated: true)
+                            self.dismiss(animated: true){
+                                self.delegate?.didSaveNewShiori(self.dataModel)
+                            }
                         }
                     }
-                    
                     // 先にアラートを閉じてから戻る
                     if let presented = self.presentedViewController {
                         presented.dismiss(animated: true, completion: closeModal)
                     } else {
                         closeModal()
-                    }
-                }
-                
-                // ”OK”を押したら閉じて戻る
-                alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
-                    finish()
-                })
-                
-                self.present(alert, animated: true){
-                    // 2秒後に未操作なら自動で閉じて戻る
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                        finish()
                     }
                 }
             }
