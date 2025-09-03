840835a28c8e582dd3f5cbeaf7f9bc60a0d19e0e
TAMURA IBUKI <ibu0414@yahoo.co.jp>
2025-09-04 01:04:38 +0900

    しおり情報修正後のメッセージ表示時間の修正

--- TabishioriApp/View/EditShioriViewController.swift
+++ TabishioriApp/View/EditShioriViewController.swift
@@ -380,7 +380,7 @@ final class EditShioriViewController: UIViewController {
                 let alert = UIAlertController(title: "更新しました", message: nil, preferredStyle: .alert)
                 self.present(alert, animated: true)
                 
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
                     guard let self = self else { return }
                     let closeModal: () -> Void = {
                         if let nav = self.navigationController {
